d18a36aa49359f554ec7a89e37e465bab67faf9e
LeandroViggiano <leandrom.v@outlook.com>
2024-08-06 17:12:39 -0300

    primer commit, front de navbar creado

---
+++ e-commerce-gym/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Home, Search, Bell, Star, User } from 'react-feather';
+import styles from './Navbar.module.css';
+
+const NavItem = ({ icon, text, active = false, notifications = 0 }) => (
+  <span className={`${styles.navItem} ${active ? styles.active : ''}`}>
+    {icon}
+    <span className={styles.text}>{text}</span>
+    {notifications > 0 && (
+      <span className={styles.notification}>
+        <span className={styles.notificationInner}>{notifications}</span>
+      </span>
+    )}
+  </span>
+);
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.menu} id="nav">
+        <NavItem icon={<Home className={styles.icon} />} text="Home" active />
+        <NavItem icon={<Search className={styles.icon} />} text="Search" />
+        <NavItem icon={<Bell className={styles.icon} />} text="Notifications" notifications={13} />
+        <NavItem icon={<Star className={styles.icon} />} text="Favorites" />
+        <NavItem icon={<User className={styles.icon} />} text="Your Profile" notifications={1} />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
